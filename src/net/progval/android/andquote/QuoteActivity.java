51db7dc683e693956408049290ad313be2277621
Valentin Lorentz <progval@progval.net>
2012-08-17 16:01:32 +0200

    Add (configurable) navigation between quotes on image-based websites.

--- src/net/progval/android/andquote/QuoteActivity.java
+++ src/net/progval/android/andquote/QuoteActivity.java
@@ -7,18 +7,28 @@ import android.text.Html;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+import android.view.KeyEvent;
+import android.widget.Button;
 import android.webkit.WebView;
+import android.view.ViewGroup;
+import android.text.InputType;
 import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.ScrollView;
+import android.app.ProgressDialog;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.content.SharedPreferences;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
 import android.preference.PreferenceManager;
+import android.widget.TextView.OnEditorActionListener;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
-public class QuoteActivity extends Activity {
+public class QuoteActivity extends Activity implements OnClickListener {
     private static SharedPreferences settings; 
     private LinearLayout layout;
 
@@ -28,7 +38,8 @@ public class QuoteActivity extends Activity {
 
     private TextView contentview, scoreview;
     private WebView imageview;
-    private LinearLayout comments;
+    private LinearLayout comments, navigation;
+    private EditText gotopage;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -60,6 +71,12 @@ public class QuoteActivity extends Activity {
         }
         this.layout.setOrientation(this.layout.VERTICAL);
 
+        if (!quote.getImageUrl().equals("null") &&
+                this.settings.getBoolean("nav.img_enable", false))
+            // As images cannot be rendered in a list, it is useful to switch
+            // from a "quote" to another.
+            this.inflateNavigation();
+
         this.contentview = new TextView(this);
         this.contentview.setText(Html.fromHtml(this.quote.getContent()));
         this.layout.addView(this.contentview);
@@ -69,6 +86,97 @@ public class QuoteActivity extends Activity {
         this.layout.addView(this.scoreview);
 
         this.fetchExtraData();
+    }
+    private void inflateNavigation() {
+        this.navigation = new LinearLayout(this);
+        Button prev = new Button(this);
+        prev.setText("<=");
+        prev.setTag("prev");
+        prev.setOnClickListener(this);
+
+        Button next = new Button(this);
+        next.setText("=>");
+        next.setTag("next");
+        next.setOnClickListener(this);
+        
+        this.gotopage = new EditText(this);
+        this.gotopage.setImeOptions(EditorInfo.IME_ACTION_GO);
+        this.gotopage.setInputType(InputType.TYPE_CLASS_NUMBER);
+        this.layout.setDescendantFocusability(ViewGroup.FOCUS_BEFORE_DESCENDANTS);
+        this.layout.setFocusableInTouchMode(true);
+        this.gotopage.setText(String.format("%d", quote.getId()));
+        this.gotopage.setOnEditorActionListener(
+            new OnEditorActionListener() {
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                    boolean handled = false;
+                    if (actionId == EditorInfo.IME_ACTION_GO) {
+                        try {
+                            int id = Integer.parseInt(((EditText) v).getText().toString());
+                            QuoteActivity.this.loadQuote(id);
+                            handled = true;
+                        }
+                        catch (NumberFormatException e) {
+                                // Never trust user input
+                        }
+                    }
+                    return handled;
+                }
+            });
+
+        this.navigation.addView(prev);
+        this.navigation.addView(this.gotopage);
+        this.navigation.addView(next);
+        this.layout.addView(this.navigation);
+    }
+    public void loadQuote(int id) {
+        final ProgressDialog dialog = ProgressDialog.show(QuoteActivity.this, "", this.getResources().getString(R.string.siteactivity_loading_quotes), true);
+        class QuoteRenderer implements OpenQuoteApi.ProgressListener {
+            private OpenQuoteApi api;
+            public QuoteRenderer(OpenQuoteApi api) {
+                this.api = api;
+            }
+            public void onProgressUpdate(int progress) {}
+            public void onFail(int status_message) {
+                dialog.dismiss();
+                Toast.makeText(QuoteActivity.this, status_message, Toast.LENGTH_LONG).show();
+            }
+            public void onSuccess(String file) {
+                try {
+                    JSONObject object = (JSONObject) new JSONTokener(file).nextValue();
+                    if (!object.has("quote"))
+                        this.onFail(R.string.quoteactivity_doesnotexist);
+                    else {
+                        OpenQuoteApi.Quote quote = new OpenQuoteApi.Quote((JSONObject) object.get("quote"));
+                        QuoteActivity.this.loadQuote(quote);
+                    }
+                }
+                catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                dialog.dismiss();
+            }
+        }
+        this.api.safeGet(new QuoteRenderer(this.api),
+            String.format("/%s/quotes/show/%d/", this.state.site_id, id));
+    }
+    public void onClick(View v) {
+        String tag = (String) ((Button) v).getTag();
+        if (tag.equals("prev"))
+            this.loadQuote(this.quote.getId()-1);
+        else if (tag.equals("next"))
+            this.loadQuote(this.quote.getId()+1);
+    }
+    public void loadQuote(OpenQuoteApi.Quote quote) {
+        this.quote = quote;
+        this.contentview.setText(Html.fromHtml(this.quote.getContent()));
+        this.scoreview.setText(this.quote.getScore());
+        this.setTitle(this.state.site_name + " - " + this.quote.getId());
+        if (quote.getImageUrl() != null) {
+            this.setImage(quote.getImageUrl());
+        }
+        if (!quote.getImageUrl().equals("null") &&
+                this.settings.getBoolean("nav.img_enable", false))
+            this.gotopage.setText(String.format("%d", quote.getId()));
     }
     private void fetchExtraData() {
         class QuoteLoader implements OpenQuoteApi.ProgressListener {
